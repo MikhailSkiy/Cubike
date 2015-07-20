cbacd153527f126e322d8d98412fcef6ecc022eb
MikhailSkiy <Mikhail.Skiy@gmail.com>
2015-07-20 23:40:08 +0400

    Initial commit

---
+++ app/src/main/java/com/example/mikhail/cubike/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.mikhail.cubike;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.example.mikhail.cubike.adapters.TrackAdapter;
+import com.example.mikhail.cubike.model.Track;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        List<Track> tracks = new ArrayList<Track>();
+
+       Track ranevsckiyTrack = new Track("Таганрог Раневской","Отличный маршрут для тех, кто хочет узнать больше о жизни актрисы",2,15);
+        Track ranevsckiyTrack2 = new Track("Таганрог Чехова","Места, где А.П. Чехов провел молодость и зрелую часть жизни",1,1);
+        tracks.add(ranevsckiyTrack);
+        tracks.add(ranevsckiyTrack2);
+        ListView listView = (ListView)this.findViewById(R.id.track_list);
+        TrackAdapter trackAdapter = new TrackAdapter(this,tracks);
+        listView.setAdapter(trackAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/mikhail/cubike/model/Track.java
@@ -0,0 +1,59 @@
+package com.example.mikhail.cubike.model;
+
+/**
+ * Created by Mikhail on 19.07.2015.
+ */
+public class Track {
+    private String title;
+    private String description;
+    private double duration;
+    private int length;
+    private int rating;
+
+    public Track(String title, String description, double duration, int length) {
+        this.title = title;
+        this.description = description;
+        this.duration = duration;
+        this.length = length;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
