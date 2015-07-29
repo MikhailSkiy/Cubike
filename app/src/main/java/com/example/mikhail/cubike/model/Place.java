7570c2e92c201ced15461917ee336d21a4aef7fc
MikhailSkiy <Mikhail.Skiy@gmail.com>
2015-07-30 00:04:35 +0400

    Created table classes for place and track

--- app/src/main/java/com/example/mikhail/cubike/model/Place.java
+++ app/src/main/java/com/example/mikhail/cubike/model/Place.java
@@ -5,11 +5,12 @@ package com.example.mikhail.cubike.model;
  */
 public class Place {
     private String title_;
-    private String description_;
+    private String shortDescription_;
+    private byte[] icon_;
 
     public Place(String title,String description){
         this.title_ = title;
-        this.description_ = description;
+        this.shortDescription_ = description;
     }
 
     public void setTitle(String title){
@@ -21,10 +22,18 @@ public class Place {
     }
 
     public void setDescription(String description){
-        this.description_ = description;
+        this.shortDescription_ = description;
     }
 
     public String getDescription(){
-        return this.description_;
+        return this.shortDescription_;
+    }
+
+    public byte[] getIcon() {
+        return icon_;
+    }
+
+    public void setIcon(byte[] icon) {
+        this.icon_ = icon;
     }
 }
